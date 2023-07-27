e7fc76d2350b774315b40124474af658cc6503d1
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2023-07-27 09:29:19 +0200

    Silence a clang-tidy clang-analyzer-deadcode.DeadStores warning.

--- bzlib.c
+++ bzlib.c
@@ -448,6 +448,7 @@ int BZ_API(BZ2_bzCompress) ( bz_stream *strm, int action )
          if (s->avail_in_expect != s->strm->avail_in)
             return BZ_SEQUENCE_ERROR;
          progress = handle_compress ( strm );
+         (void) progress; /* FIXME/TODO: is a check missing here ? */
          if (s->avail_in_expect > 0 || !isempty_RL(s) ||
              s->state_out_pos < s->numZ) return BZ_FLUSH_OK;
          s->mode = BZ_M_RUNNING;
